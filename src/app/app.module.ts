d42aea1ef527b0ef4ad28e8fd4e6f98627ad7c53
Iraj Mashar <lrinchan.kagamine@gmail.com>
2020-09-08 18:53:17 +0200

    implementing toast with toastr

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,4 @@
-import { ToastModule } from './toast/toast.module';
+
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import {FormsModule} from '@angular/forms'
@@ -8,7 +8,7 @@ import { AppComponent } from './app.component';
 import { WaifusComponent } from './waifus/waifus.component';
 import { WaifuDetailComponent } from './waifu-detail/waifu-detail.component';
 import { MessagesComponent } from './messages/messages.component';
-import { ToastComponent } from './toast/toast.component';
+
 
 @NgModule({
   declarations: [
@@ -21,7 +21,6 @@ import { ToastComponent } from './toast/toast.component';
     BrowserModule,
     AppRoutingModule,
     FormsModule,
-    ToastModule
   ],
   providers: [],
   bootstrap: [AppComponent]
